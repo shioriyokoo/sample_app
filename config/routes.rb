2c6ce3d4c48dd4115ed2d91afe971ce4b21cd044
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2022-09-23 07:56:49 +0000

    [Finish] 3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
